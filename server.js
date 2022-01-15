801dd1fe51612445595d4c93b1df4e03c2c09056
Ross Scharbach <rscharbach1@my.bcit.ca>
2022-01-14 16:23:48 -0800

    add get/post

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 const express = require('express')
+const res = require('express/lib/response')
 const app = express()
 
-app.get("/", (req, res) => {
-  res.send("<h1>hello Ross</h1>")
+app.use(express.json())
+
+const skis = [
+    {
+        id: 1,
+        brand: "K2",
+        model: "Kung Fuja",
+        type: "powder",
+        length: 175
+    }
+]
+
+app.get("/api/skis", (req, res) => {
+  res.send({skis: skis})
+})
+
+app.post("/api/skis", (req,res) => {
+    const data = req.body
+    data.id = skis.length+1
+    skis.push(data)
+    res.send(data)
 })
 
 const port = process.env.PORT || 8080
