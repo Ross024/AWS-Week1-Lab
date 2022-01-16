fda7ad4abb99c21d13e2005837a6255356719725
Ross Scharbach <rscharbach1@my.bcit.ca>
2022-01-16 12:21:55 -0800

    added React build folder

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const res = require('express/lib/response')
 const app = express()
-
 app.use(express.json())
+app.use(express.static('build'))
 
 const books = ["Fires of Heaven", "Three Day Road", "The Hobbit"]
 
@@ -15,6 +15,10 @@ app.post("/api/books", (req,res) => {
     books.push(bookName)
     res.send(req.body)
 })
+
+app.get('*', (req, res) => {
+    res.sendFile('build/index.html');
+  });
 
 const port = process.env.PORT || 8090
 app.listen(port, () => console.log(`listening on port ${port}`))
